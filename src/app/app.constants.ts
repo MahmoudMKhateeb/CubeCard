d2b9a19b8cae1815d95e9551cc9e924eb008f911
Mahmoud <mhmoudoool.ive@gmail.com>
2024-12-23 13:38:06 +0200

    fix api issue

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
 
-    public static readonly apiUrl: string = 'http://localhost:8000/api/';
+    public static readonly apiUrl: string = 'http://localhost:8000/api/v1/';
     public static readonly imagesUrl: string = 'http://localhost:8000/storage';
 
 }
